c4db5ad6d9d0fee94218fcf6902364a09a8a0360
Marlon Rosa <114532602+marlongrosa@users.noreply.github.com>
2025-05-13 05:30:36 +0000

    Alterações

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+// substitua 'seu-usuario' e 'repositorio' abaixo
+export default defineConfig({
+  base: '/auto-proposta-hub-react/', // 👈 nome do repo
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-}));
+})
